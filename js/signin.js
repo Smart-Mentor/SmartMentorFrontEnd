e4709186e79db7419bff16f1552d0fdbb96c020b
Elamawy-1 <amrelamawy712@gmail.com>
2025-12-02 23:50:58 +0200

    login page

--- js/signin.js
+++
@@ -1,9 +0,0 @@
-let passwordInput = document.getElementById("password");
-let showIcon = document.getElementById("icone");
-
-showIcon.onclick = () => {
-    let isPassword = passwordInput.type === "password";
-    passwordInput.type = isPassword ? "text" : "password";
-    showIcon.classList.toggle("fa-eye");
-    showIcon.classList.toggle("fa-eye-slash");
-};
